5edc069b1b7f6fbf4a13be3a61df78a182052105
cujba-mihai-indrivo <mihai.cujba@indrivo.com>
2024-10-20 20:37:18 +0300

    Removed wait for db in startup script

--- startup.relational.dev.sh
+++ startup.relational.dev.sh
@@ -1,20 +1,7 @@
 #!/usr/bin/env bash
 set -e
 
-# Extract host and port from DATABASE_URL
-if [ -n "$DATABASE_URL" ]; then
-  # Parse the DATABASE_URL for host and port
-  DB_HOST=$(echo $DATABASE_URL | sed -e 's,.*//[^@]*@\(.*\):\([0-9]*\)/.*,\1,')
-  DB_PORT=$(echo $DATABASE_URL | sed -e 's,.*//[^@]*@\(.*\):\([0-9]*\)/.*,\2,')
-else
-  echo "DATABASE_URL not set."
-  exit 1
-fi
-
-# Wait for the database to be ready
-/opt/wait-for-it.sh $DB_HOST:$DB_PORT
-
-# Run migrations and seeds
+# /opt/wait-for-it.sh postgres:5432
 npm run migration:run
 npm run seed:run:relational
 npm run start:prod
