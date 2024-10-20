71c2c442566617d1c702ad464cd98a52be66c579
cujba-mihai-indrivo <mihai.cujba@indrivo.com>
2024-10-20 20:40:57 +0300

    fix: removed setting env from env-example-relational

--- startup.relational.dev.sh
+++ startup.relational.dev.sh
@@ -1,7 +1,25 @@
 #!/usr/bin/env bash
 set -e
 
-# /opt/wait-for-it.sh postgres:5432
+# Log the environment variables
+echo "DATABASE_URL: $DATABASE_URL"
+echo "NODE_ENV: $NODE_ENV"
+
+# Extract host and port from DATABASE_URL (provided by Heroku)
+if [ -n "$DATABASE_URL" ]; then
+  DB_HOST=$(echo $DATABASE_URL | sed -e 's,.*//[^@]*@\(.*\):\([0-9]*\)/.*,\1,')
+  DB_PORT=$(echo $DATABASE_URL | sed -e 's,.*//[^@]*@\(.*\):\([0-9]*\)/.*,\2,')
+else
+  echo "DATABASE_URL not set."
+  exit 1
+fi
+
+# Wait for the database to be ready
+/opt/wait-for-it.sh $DB_HOST:$DB_PORT
+
+# Run migrations and seeds
 npm run migration:run
 npm run seed:run:relational
+
+# Start the production app
 npm run start:prod
