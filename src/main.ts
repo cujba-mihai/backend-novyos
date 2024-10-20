3f44bf2e3a36a64cf0c17390b70b2704886214c9
cujba-mihai-indrivo <mihai.cujba@indrivo.com>
2024-10-20 21:13:44 +0300

    Listen to port

--- src/main.ts
+++ src/main.ts
@@ -46,6 +46,8 @@ async function bootstrap() {
   const document = SwaggerModule.createDocument(app, options);
   SwaggerModule.setup('docs', app, document);
 
-  await app.listen(configService.getOrThrow('app.port', { infer: true }));
+  await app.listen(
+    process.env.PORT || configService.getOrThrow('app.port', { infer: true }),
+  );
 }
 void bootstrap();
