7a08592a065cd4d139a69d1cfaa68609bfb1af96
cujba-mihai-indrivo <mihai.cujba@indrivo.com>
2024-10-20 19:45:22 +0300

    fix: postgres command not recognized

--- startup.relational.dev.sh
+++ startup.relational.dev.sh
@@ -1,7 +1,17 @@
 #!/usr/bin/env bash
 set -e
 
-/opt/wait-for-it.sh postgres:5432
+if [ -n "$DATABASE_URL" ]; then
+  DB_HOST=$(echo $DATABASE_URL | sed -e 's,.*//\(.*\):\(.*\)@.*,\1,')
+  DB_PORT=5432
+else
+  echo "DATABASE_URL not set."
+  exit 1
+fi
+
+/opt/wait-for-it.sh $DB_HOST:$DB_PORT
+
+# Run migrations and seeds
 npm run migration:run
 npm run seed:run:relational
 npm run start:prod
