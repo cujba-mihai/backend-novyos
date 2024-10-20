1b6ec6f7ea7e8cb5704b99f212927265f30a79bd
cujba-mihai-indrivo <mihai.cujba@indrivo.com>
2024-10-20 19:51:14 +0300

    fix: database name command not recognized

--- startup.relational.dev.sh
+++ startup.relational.dev.sh
@@ -1,14 +1,17 @@
 #!/usr/bin/env bash
 set -e
 
+# Extract host, port, and database from the DATABASE_URL
 if [ -n "$DATABASE_URL" ]; then
-  DB_HOST=$(echo $DATABASE_URL | sed -e 's,.*//\(.*\):\(.*\)@.*,\1,')
-  DB_PORT=5432
+  # Parse the DATABASE_URL
+  DB_HOST=$(echo $DATABASE_URL | sed -e 's,.*//[^@]*@\(.*\):\([0-9]*\)/.*,\1,')
+  DB_PORT=$(echo $DATABASE_URL | sed -e 's,.*//[^@]*@\(.*\):\([0-9]*\)/.*,\2,')
 else
   echo "DATABASE_URL not set."
   exit 1
 fi
 
+# Wait for the database to be ready
 /opt/wait-for-it.sh $DB_HOST:$DB_PORT
 
 # Run migrations and seeds
